9110af6c46944e501151398a324977e2e058ddd4
AA-C-hub <aacc3@hawaii.edu>
2025-10-01 07:52:30 -1000

    ye

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    //GLOBAL FUNCTIONS GO HERE
+document.addEventListener('DOMContentLoaded', () => {
+
+    //ENABLE MOBILE MENU BUTTON
+    const sitenav = document.querySelector(".site-nav");
+    const menubutton = document.querySelector(".menubutton");
+
+    console.log(sitenav);
+    console.log(menubutton);
+
+    menubutton.onclick = () => {
+  
+    // IF MENU IS CLOSED, OPEN IT, ELSE CLOSE IT
+    if (sitenav.getAttribute("data-menustate") === "closed") {
+    sitenav.setAttribute("data-menustate", "open");
+    } else {
+    sitenav.setAttribute("data-menustate", "closed");
+    };
+  
+    };
     
 });
